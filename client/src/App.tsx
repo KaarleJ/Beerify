063eb301a7da4657a33598de1cbd52d62b1b8284
Kaarle Järvinen <kaarle46@gmail.com>
2024-04-17 00:20:21 +0300

    ENabled shadcn/ui, built basic structure of frontend view and enabled react-router-dom.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,10 @@
-import { useEffect, useState } from 'react';
-import viteLogo from '/vite.svg';
-import { getCount } from './services/api';
-
-function App() {
-  const [serverCount, setServerCount] = useState(0);
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getCount();
-      setServerCount(data.count);
-    };
-    fetchData();
-  }, []);
-
-
+import { ReactNode } from 'react';
+import NavBar from './components/NavBar';
+function App({ children }: { children: ReactNode }) {
   return (
-    <div className='flex flex-col min-h-screen items-center justify-start'>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="m-12" alt="Vite logo" />
-        </a>
-      </div>
-      <h1 className='text-blue font-sans text-2xl'>Beerify - initial app</h1>
-      <div className="m-5">
-        <button type='button' onClick={() => setCount((count) => count + 1)}>
-          Clientside count is {count}
-        </button>
-        <p>
-          Serverside count is {serverCount}
-        </p>
-      </div>
+    <div className="flex flex-col justify-center items-center mt-50">
+      <NavBar />
+      <div className='mt-28'>{children}</div>
     </div>
   );
 }
---
+++ client/src/views/Error.tsx
@@ -0,0 +1,10 @@
+const Error = () => {
+  return (
+    <div className="text-center text-primary">
+      <h1>404</h1>
+      <h2>The page you're looking for couldn't be found</h2>
+    </div>
+  );
+};
+
+export default Error;
