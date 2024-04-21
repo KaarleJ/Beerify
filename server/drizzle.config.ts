9ba0058429c5aa5b5027784edceadfe68bd51e51
Kaarle Järvinen <kaarle46@gmail.com>
2024-04-21 15:12:42 +0300

    Fixed project a bit.

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -5,7 +5,7 @@ export default {
   out: './drizzle',
   driver: 'pg', // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL as string,
   },
   verbose: true,
   strict: true,
--- server/types.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace Express {
-    interface Request {
-      user?: JwtPayload | string | object | null;
-    }
-  }
-}
