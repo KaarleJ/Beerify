57fb07e156220f9194317e6f540bd77142e59ac8
Kaarle Järvinen <kaarle46@gmail.com>
2024-04-23 20:09:23 +0300

    Fixed schema path in drizzleconfig

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
 export default {
-  schema: './database/schema.ts',
+  schema: './src/database/schema.ts',
   out: './drizzle',
   driver: 'pg', // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
   dbCredentials: {
