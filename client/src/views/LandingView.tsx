80f58597e4b80e891e8f6cc5ff659cbf1dd6dd30
Kaarle Järvinen <kaarle46@gmail.com>
2024-11-17 12:59:19 +0200

    Changed landing style

--- client/src/views/LandingView.tsx
+++ client/src/views/LandingView.tsx
@@ -1,25 +1,37 @@
+import { Button } from '@/components/ui/button';
+
 const LandingView = () => {
   return (
     <div className="w-full h-screen flex flex-col items-stretch">
-      <div className="bg-slate-300 bg-bottom bg-cover bg-[url('/beer.jpg')] h-full text-center">
-        <h1 className="pt-32 pl-0">
-          Meet Beerify, <br></br> the most rushed beer rating app
-        </h1>
+      <div className="bg-slate-300 bg-bottom bg-cover bg-[url('/beer.jpg')] h-[15rem] text-center">
+        <h1 className="py-32 pl-64 text-left text-6xl">Beerify</h1>
       </div>
-      <div className="h-full p-12 flex flex-col items-center">
-        <div className="max-w-4xl w-full p-5">
-          <h2 className="px-10 pb-5">
-            With beerify you can rate and review beers you have tasted
+      <div className="flex items-center justify-between px-64 py-12 gap-12">
+        <div className="flex flex-col justify-start h-full">
+          <h2 className="text-2xl pb-10">
+            Meet Beerify, the most rushed beer rating app
           </h2>
-          <p className="text-xl">
+          <p className="text-lg max-w-2xl">
             Beerify is the home (or at least will be) of thousands of beer
             reviews from passionate beer lovers just like you. Lost in the beer
             aisle? Don't worry! Open Beerify and discover your perfect brew!
           </p>
-          <p className="text-center p-5 text-xl">
-            Get started by <a href="/login">logging in</a> or{' '}
-            <a href="register">registering</a>
+          <p className="text-left pt-10 text-xl">
+            Get started by
+            <Button asChild className="text-lg mx-5">
+              <a href="/login">Logging in</a>
+            </Button>
+            or
+            <Button asChild className="text-lg mx-5">
+              <a href="register">Registering</a>
+            </Button>
           </p>
+        </div>
+        <div className="h-[20rem] w-[50rem] overflow-hidden rounded-md shadow-xl shadow-primary shado">
+          <img
+            src="https://images.unsplash.com/photo-1438557068880-c5f474830377?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="object-contain object-center"
+          />
         </div>
       </div>
     </div>
